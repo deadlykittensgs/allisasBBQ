517e02fab4e52e57d0e43031a5b8b1e54f29e4a5
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-07-16 11:45:00 -0500

    work on reviews dots and where forms go when subd

--- src/Pages/Catering.jsx
+++ src/Pages/Catering.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import Logo from '../assets/logo.png';
 
 export default function Catering() {
-  return (
+  
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [event, setEvent] = useState('');
+    const [guestCount, setGuestCount] = useState('');
+    const [date, setDate] = useState('');
+    const [message, setMessage] = useState('Tell us about your event and we will be in touch!');
+  
+  
+   const handleClick = () => {
+
+if (name ==="" || phone ==="" || email ==="" || event ==="" || guestCount ==="" || date ==="") {
+    setMessage("Please fill out all forms below")
+    return 
+} else {
+    alert(`name: ${name} phone ${phone} email ${email} event ${event} guestCount ${guestCount} date ${date}`);
+    setMessage("Catering Request Sent!") 
+   }};
+  
+  
+  
+    return (
+
     <>
      <Header/>
 
@@ -19,45 +42,45 @@ export default function Catering() {
 
         <div className='flex flex-col items-center'> 
             <p className='text-[2rem]'>Catering </p>
-            <p className=''>Tell us about your event and we will be in touch! </p>
+            <p className={message === "Catering Request Sent!" ? 'text-red-600 text-[2rem]': 'text-slate-100 text-[1.3rem] text-slate-950' }>{message} </p>
         </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Name</p>
-    <input type="text" name="" id="" placeholder='e.g John Doe'/>
+    <input required type="text"  value={name} onChange={(e) => setName(e.target.value)} placeholder='e.g John Doe'/>
 </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Phone</p>
-    <input type="text" name="" id="" placeholder='e.g (123) 456-789'/>
+    <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='e.g (123) 456-789'/>
 </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Email</p>
-    <input className='' type="email" name="" id="" placeholder='e.g jondoe@example.com'/>
+    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='e.g jondoe@example.com'/>
 </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Event</p>
-    <input type="text" name="" id="" placeholder='e.g wedding/graduation '/>
+    <input type="text" value={event} onChange={(e) => setEvent(e.target.value)}  placeholder='e.g wedding/graduation '/>
 </div>
 
 <div className='flex flex-col' >
     <p>Guest Count Estimate:</p>
    
-    <input type="text" name="" id="" placeholder='e.g 100 adults 10 kids' />
+    <input type="text" value={guestCount} onChange={(e) => setGuestCount(e.target.value)} placeholder='e.g 100 adults 10 kids' />
     </div>
   
 
     <div className='flex flex-col '>
     <p>Date Desired / Additional notes</p>
-    <textarea className='h-[200px]' type="text" name="" id="" placeholder=' Tell us when you need catering and anything else you think we should know' />
+    <textarea className='h-[200px]' type="text" value={date} onChange={(e) => setDate(e.target.value)}  placeholder=' Tell us when you need catering and anything else you think we should know' />
     </div>
 
 
     <div className='flex justify-between p-3'>
         <button className='bg-slate-300 rounded p-1 border border-slate-800 hover:bg-slate-400'>Back</button>
-        <button className='bg-slate-300 rounded p-1 border border-slate-800 hover:bg-slate-400'>Submit</button>
+        <button onClick={handleClick} className='bg-slate-300 rounded p-1 border border-slate-800 hover:bg-slate-400'>Submit</button>
     </div>
     
     </div>
--- src/Pages/Jobs.jsx
+++ src/Pages/Jobs.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import Logo from '../assets/logo.png';
 
 export default function Jobs() {
+
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [experience, setExperience] = useState('');
+    const [position, setPosition] = useState('');
+    const [Resume, setResume] = useState('');
+    const [coverLetter, setCoverLetter] = useState('');
+    const [message, setMessage] = useState('Tell us a little about yourself and we will be in touch!')
+
+    const handleClick = () => {
+
+        if (name ==="" || phone ==="" || email ==="" || experience ==="" || position ==="" || coverLetter ==="") {
+            setMessage("Please fill out all forms below")
+            return 
+        } else {
+            alert(`name: ${name} phone ${phone} email ${email} experience ${experience} position ${position} Resume ${Resume} coverLetter ${coverLetter}`);
+            setMessage("Application Sent!") 
+           }};
+  
+
+
   return (
     <>
      <Header/>
@@ -19,45 +41,51 @@ export default function Jobs() {
 
         <div className='flex flex-col items-center'> 
             <p className='text-[2rem]'>Want to work with us? </p>
-            <p className=''>Tell us a little about yourself and we will be in touch! </p>
+            <p className=''>{message} </p>
         </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Name</p>
-    <input type="text" name="" id="" placeholder='e.g John Doe'/>
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)}  placeholder='e.g John Doe'/>
 </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Phone</p>
-    <input type="text" name="" id="" placeholder='e.g (123) 456-789'/>
+    <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='e.g (123) 456-789'/>
 </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>Email</p>
-    <input className='' type="email" name="" id="" placeholder='e.g jondoe@example.com'/>
+    <input  type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='e.g jondoe@example.com'/>
 </div>
 
 <div className='flex flex-col'>
     <p className='text-[1.7rem]'>e.g Experience</p>
-    <input type="text" name="" id="" placeholder='e.g 3 years serving'/>
+    <input type="text" value={experience} onChange={(e) => setExperience(e.target.value)} placeholder='e.g 3 years serving'/>
 </div>
 
 <div className='flex flex-col' >
     <p>Applying for:</p>
    
-    <input type="text" name="" id="" placeholder='e.g waiter/cook' />
+    <input type="text" value={position} onChange={(e) => setPosition(e.target.value)} placeholder='e.g waiter/cook' />
+    </div>
+
+    <div className='flex flex-col' >
+    <p>Upload Resume:</p>
+   
+    <input type="text" />
     </div>
   
 
     <div className='flex flex-col '>
     <p>Cover Letter</p>
-    <textarea className='h-[200px]' type="text" name="" id="" placeholder=' Write a short comment about your skills and experience' />
+    <textarea className='h-[200px]' type="text" value={coverLetter} onChange={(e) => setCoverLetter(e.target.value)} placeholder=' Write a short comment about your skills and experience' />
     </div>
 
 
     <div className='flex justify-between p-3'>
         <button className='bg-slate-300 rounded p-1 border border-slate-800 hover:bg-slate-400'>Back</button>
-        <button className='bg-slate-300 rounded p-1 border border-slate-800 hover:bg-slate-400'>Submit</button>
+        <button onClick={handleClick} className='bg-slate-300 rounded p-1 border border-slate-800 hover:bg-slate-400'>Submit</button>
     </div>
     
     </div>
